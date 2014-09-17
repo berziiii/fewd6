f26e30ab36ab5fd94694808fbd6a0cb111a3ed5a
brian.berzellini <berzelba@Macintosh.local>
2014-09-17 16:04:52 -0400

    JavaScript homework
    Here is my Javascript homework. I was SERIOUSLY struggling with how to
    input the src=posts.image but with ALOT of research and looking on the
    Google I realized that I was not separating the ‘ and + correctly. Had
    one of those AHH-HA moments.

--- week_5_advanced_js_jquery/my_cat_blog/js/index.js
+++ week_5_advanced_js_jquery/my_cat_blog/js/index.js
@@ -1,3 +1,4 @@
+var html = '';
 var posts = [
 	{
 		image: 'http://placekitten.com/g/200/200',
@@ -6,14 +7,35 @@ var posts = [
 	}, {
 		image: 'http://placekitten.com/g/200/200',
 		title: 'Check out these cute cats!',
-		snippet: 'Wow...thats all I can say. I was surfing /cats on reddit and came across the cutest cat pictures yet! Take a look!'
+		snippet: 'Wow...thats all I can say. I was surfing cats on reddit and came across the cutest cat pictures yet! Take a look!'
 	}, {
 		image: 'http://placepu.gs/200/200',
-		title: 'Wierd cats!',
+		title: 'Weird cats!',
 		snippet: 'I saw these weird looking cats on the interwebs today. If you can tell me what breed they are, please do so in the comments!'
 	}, {
 		image: 'http://placekitten.com/g/200/200',
-		title: 'Cutness overload!',
+		title: 'Cuteness overload!',
 		snippet: 'OMG people, are you ready for some heavy duty cuteness? I know I am. Check out these cats! I can\'t even handle this. Too cute!'
 	}
-];
+]
+
+function generateBlogSection(posts) {
+var ret = '<div class="post clearfix">'
+ret += '<img class="post-image float-left" src="' + posts.image + '">';
+ret += '<div class="post-content float-left">';
+ret += '<h3 class="post-title">' + posts.title + '!' + '</h3>';
+ret += '<p>';
+ret += posts.snippet;
+ret += '</p>';
+ret += '</div>';
+ret += '</div>';
+return ret;
+}
+
+
+for (var i = 0; i < posts.length; i++) {
+	html += generateBlogSection(posts[i]);
+}
+
+document.getElementById("posts").innerHTML = html
+
